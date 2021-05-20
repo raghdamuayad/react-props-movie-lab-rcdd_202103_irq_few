bead74a611fd582530b4ff36b8f928de4f786f31
Raghda  Muayad <therose000777@gmail.com>
2021-05-20 10:05:55 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -20,7 +20,7 @@ export default class CardBack extends Component {
   render() {
     return (
       <div className="card-back">
-      
+
       <h3 className="title">{this.props.title}</h3>
         <span />
       {this.generateRatingElement()}
